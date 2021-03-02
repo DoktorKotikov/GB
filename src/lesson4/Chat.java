bac736d57f114d6f769777385ef8901d793a23d8
valery <eliseev.valery413@gmail.com>
2021-03-02 22:21:39 +0300

    add lesson 4

---
+++ src/lesson4/Chat.java
@@ -0,0 +1,39 @@
+package lesson4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Chat {
+    @FXML
+    public TextArea chatArea;
+    public ListView usersOnline;
+    public Button btnSendMessage;
+    public TextField textMessage;
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(1);
+    }
+
+    public void about(ActionEvent actionEvent) {
+    }
+
+    public void support(ActionEvent actionEvent) {
+    }
+
+    public void pressEnter(ActionEvent actionEvent) {
+    }
+
+    public void btnSend(ActionEvent actionEvent) {
+
+    }
+}
